47d83bb75db68547b1fe91893c677823ef3d3b49
Viktor Vodnev <v.vodnev73@gmail.com>
2024-05-20 15:20:42 +0200

    Add logging to the keepass backup script

--- keepass_backup.sh
+++ keepass_backup.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 source_dir="/home/vv173/MEGA/PassBackup/"
 destination_dir="/home/vv173/.keepass"
+log_file="/var/log/keepass_backup.log"
 
-/usr/bin/rsync -av "$source_dir" "$destination_dir"
+set -x
+
+logger -t "keepass_backup" -p user.info "Backup script started."
+
+echo "! -- $(date +"%d-%m-%Y %H:%M:%S")" >> ${log_file}
+
+/usr/bin/rsync -av "$source_dir" "$destination_dir" >> ${log_file} 2>&1
+
+logger -t "keepass_backup" -p user.info "Backup script ended."
